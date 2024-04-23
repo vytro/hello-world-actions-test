a7b118eb3ac7de72a91b161c150407932ced7d5e
vytro <brayan.bbgc@gmail.com>
2024-04-23 11:51:37 -0400

    editado variables

--- hello-test.py
+++ hello-test.py
@@ -4,6 +4,8 @@ def hello():
     # print("Hello, World!")
     message = os.environ.get("A_VARIABLE", "World")
     print(f"Hello, {message}!")
+    print(f"Se puede ver el secreto: {os.environ.get('A_VARIABLE', 'no hay')}")
+    print(f"Hello, {os.environ.get("B_VARIABLE", "World")}!")
 
 if __name__ == "__main__":
     hello()
