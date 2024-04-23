a9e65110bc733f91a07a6cc30fa8c280986c1bad
vytro <brayan.bbgc@gmail.com>
2024-04-23 11:39:37 -0400

    edited files

--- hello-test.py
+++ hello-test.py
@@ -1,5 +1,7 @@
 def hello():
-    print("Hello, World!")
+    # print("Hello, World!")
+    message = os.environ.get("A_VARIABLE", "World")
+    print(f"Hello, {message}!")
 
 if __name__ == "__main__":
     hello()
